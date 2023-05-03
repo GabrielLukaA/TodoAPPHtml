42790171f3082a95ad2b967bb6ae6ca93a29b46f
GABRIEL LUKA DE AMARAL <gabriel_luka-amaral@estudante.sesisenai.org.br>
2023-05-03 16:56:10 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
+interface Pessoa {
+  nome: string;
+  categoria: string;
+}
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+
+  usuarios: Pessoa[] = [];
+
+  pessoa: Pessoa = {
+    nome: '',
+    categoria: ''
+  }
+  cadastrarUsuario() {
+
+    const usuario: Pessoa = {
+      nome: this.pessoa.nome,
+      categoria: this.pessoa.categoria
+
+    }
+    this.pessoa.nome = ''
+    this.usuarios.push(usuario);
+    localStorage.setItem("Usuários", JSON.stringify(this.usuarios));
+
+  }
+  removerUsuario(indice: number) {
+    this.usuarios.splice(indice, 1)
+    localStorage.removeItem("Usuários");
+    localStorage.setItem("Usuários", JSON.stringify(this.usuarios));
+  }
+  ngOnInit() {
+    const tarefas = localStorage.getItem('Usuários');
+    if (tarefas) {
+      this.usuarios = JSON.parse(tarefas);
+    }
+  }
+  esconderInput(): void {
+    this.mostraInput = false;
+  }
+  mostrarInput(): void {
+    this.mostraInput = true;
+  
+  }
+  salvar(usuario : Pessoa): void{
+    this.salvarLocalStorage()
+  }
+  salvarLocalStorage(){
+    localStorage.setItem("Usuários", JSON.stringify(this.usuarios)); 
+  }
+}
