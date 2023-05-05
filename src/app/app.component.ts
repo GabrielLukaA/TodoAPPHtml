df5fadaa21309907e0e32ab2f1c7fc6490fb395d
GABRIEL LUKA DE AMARAL <gabriel_luka-amaral@estudante.sesisenai.org.br>
2023-05-04 21:57:27 -0300

    atualizacaoKanban

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,12 @@ interface Pessoa {
 export class AppComponent {
 
 
+  trueTodo: boolean = true;
+  trueDoing: boolean = true;
+  trueDone: boolean = true;
+  usuariosDoing: Pessoa[]=[];
+  usuariosTodo: Pessoa[]=[];
+  usuariosDone: Pessoa[]=[];
   usuarios: Pessoa[] = [];
 
   pessoa: Pessoa = {
@@ -23,14 +29,35 @@ export class AppComponent {
     const usuario: Pessoa = {
       nome: this.pessoa.nome,
       categoria: this.pessoa.categoria
-
+    
     }
     this.pessoa.nome = ''
     this.usuarios.push(usuario);
     localStorage.setItem("Usuários", JSON.stringify(this.usuarios));
+    if (usuario.categoria=="to-do"){
+      this.usuariosTodo.push(usuario);
+      localStorage.setItem("UsuariosTodo", JSON.stringify(this.usuariosTodo));
+      if(this.usuariosTodo.length>1){
+        this.trueTodo= false;
+      }
+    } else if (usuario.categoria=="doing"){
+      this.usuariosDoing.push(usuario);
+      localStorage.setItem("UsuariosDoing", JSON.stringify(this.usuariosDoing));
+
+      if(this.usuariosDoing.length>1){
+        this.trueDoing= false;
+      }
+    } else if (usuario.categoria=="done"){
+      this.usuariosDone.push(usuario);
+      localStorage.setItem("UsuariosDone", JSON.stringify(this.usuariosDone));
+      if(this.usuariosDone.length>1){
+        this.trueDone= false;
+      }
+    }
 
   }
   removerUsuario(indice: number) {
+
     this.usuarios.splice(indice, 1)
     localStorage.removeItem("Usuários");
     localStorage.setItem("Usuários", JSON.stringify(this.usuarios));
@@ -40,11 +67,63 @@ export class AppComponent {
     if (tarefas) {
       this.usuarios = JSON.parse(tarefas);
     }
+    const tarefasTodo = localStorage.getItem('UsuariosTodo');
+    if (tarefasTodo) {
+      this.usuariosTodo = JSON.parse(tarefasTodo);
+    }
+    const tarefasDoing = localStorage.getItem('UsuariosDoing');
+    if (tarefasDoing) {
+      this.usuariosDoing = JSON.parse(tarefasDoing);
+    }
+    const tarefasDone = localStorage.getItem('UsuariosDone');
+    if (tarefasDone) {
+      this.usuariosDone = JSON.parse(tarefasDone);
+    }
   }
-  salvar(usuario : Pessoa): void{
-    this.salvarLocalStorage()
-  }
-  salvarLocalStorage(){
-    localStorage.setItem("Usuários", JSON.stringify(this.usuarios)); 
-  }
+  alterar(usuario : Pessoa, indice : number): void{
+
+    if(usuario.categoria=='to-do'){
+    /*  this.usuarios.splice(indice, 1)
+      localStorage.removeItem("Usuários");
+      localStorage.setItem("Usuários", JSON.stringify(this.usuarios));
+      this.usuarios.splice(indice, 1)
+      localStorage.removeItem("UsuariosDoing");
+      localStorage.setItem("UsuariosDoing", JSON.stringify(this.usuariosDoing));
+      this.usuarios.splice(indice, 1)
+      localStorage.removeItem("UsuariosDone");
+      localStorage.setItem("UsuariosDone", JSON.stringify(this.usuariosDone));*/
+      usuario.categoria= 'to-do'
+      this.usuariosTodo.push(usuario)
+      localStorage.setItem("UsuariosTodo", JSON.stringify(this.usuariosTodo))
+    } 
+   else  if(usuario.categoria=='doing'){
+      /*this.usuarios.splice(indice, 1)
+      localStorage.removeItem("Usuários");
+      localStorage.setItem("Usuários", JSON.stringify(this.usuarios));
+      this.usuarios.splice(indice, 1)
+      localStorage.removeItem("UsuariosTodo");
+      localStorage.setItem("UsuariosTodo", JSON.stringify(this.usuariosTodo));
+      this.usuarios.splice(indice, 1)
+      localStorage.removeItem("UsuariosDone");
+      localStorage.setItem("UsuariosDone", JSON.stringify(this.usuariosDone));*/
+      usuario.categoria= 'doing'
+      this.usuariosDoing.push(usuario)
+      localStorage.setItem("UsuariosDoing", JSON.stringify(this.usuariosDoing))
+    } 
+   else  if(usuario.categoria=='done'){
+      /*this.usuarios.splice(indice, 1)
+     localStorage.removeItem("Usuários");
+      localStorage.setItem("Usuários", JSON.stringify(this.usuarios));
+      this.usuarios.splice(indice, 1)
+      localStorage.removeItem("UsuariosDoing");
+      localStorage.setItem("UsuariosDoing", JSON.stringify(this.usuariosDoing));
+      this.usuarios.splice(indice, 1)
+      localStorage.removeItem("UsuariosTodo");
+      localStorage.setItem("UsuariosTodo", JSON.stringify(this.usuariosTodo));*/
+      usuario.categoria= 'done'
+      this.usuariosDone.push(usuario)
+      localStorage.setItem("UsuariosDone", JSON.stringify(this.usuariosDone))
+    } 
+     
+}
 }
