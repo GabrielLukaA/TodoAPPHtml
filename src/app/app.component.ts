b368665ea5db399f85745e0066ce78e3db0a4514
GabrielLukaA <gabriel_luka-amaral@estudante.sesisenai.org.br>
2023-06-26 18:43:05 -0300

    fixBugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { NgModel } from '@angular/forms';
-import { User } from 'src/models/users/user';
 import { UserRepository } from 'src/repositories/user.repository';
+import { User } from 'src/models/users/user';
 
 interface Tarefa {
   nome: string;
@@ -14,5 +14,37 @@ interface Tarefa {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+title = "teste-app"
 
+private userId: string = 'joao.silva';
+private users:User[] = [];
+user: User | undefined;
+
+constructor(
+  private userRepository: UserRepository
+){
+  this.users = this.userRepository.getUsers();
+  this.getUsuarioLogado();
+  console.log(this.user)
+}
+
+private getUsuarioLogado(): User {
+   return this.users.find((user) => {
+    return user.id === this.userId
+   } );
+}
+adicionarTarefa():void{
+ if(!this.hasPermission('edit')){
+  alert("Não pode cadastrar");
+  return;
+ }
+ alert("Pode Cadastrar")
+}
+
+
+private hasPermission(permission:string):Boolean{
+  return this.user.cardPermissions.some((cardPermission) => {
+return cardPermission === permission;
+  });
+}
 }
--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,12 +1,14 @@
 import { Injectable } from "@angular/core";
-import { users } from '../data/users';
+import { users } from "../data/users";
 import { User } from "src/models/users/user";
 
 @Injectable()
-export class UserRepository {
+ export class UserRepository {
+constructor(){
 
-    getUsers(): User[] {
-        return users;
+}
+    getUsers(): User[]{
+    return users;
     }
 
 }
